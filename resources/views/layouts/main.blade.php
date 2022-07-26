32c7464c5b6a7f1928784ccd24e98b4d44a23723
Tertuliano Castro <tertulianocastro@gmail.com>
2022-07-26 20:12:43 -0300

    flash messages

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -71,7 +71,25 @@
 
         </header>
 
-        @yield('content')
+        <main>
+
+            <div class="container-fluid">
+
+                <div class="row">
+
+                    @if(session('msg'))
+
+                    <p class="msg">{{ session('msg') }}</p>
+
+                    @endif
+
+                    @yield('content')
+                    
+                </div>
+
+            </div>
+
+        </main>
 
         <footer>
 
